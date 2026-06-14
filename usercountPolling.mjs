627a8c8e2631ced0e5a37a2614c5b7032c5cca28
András Simon <andrex@andrexserver.org>
2026-06-14 21:07:51 +0200

    fix fs/promises

--- usercountPolling.mjs
+++ usercountPolling.mjs
@@ -1,23 +1,23 @@
-import { writeFile } from "fs";
+import { writeFile } from "fs/promises";
 import Database from "better-sqlite3";
 import path from "path";
 import { fileURLToPath } from 'url';
-const db = new Database("shadowmail.db");
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "usercount.txt");
+
+const db = new Database("shadowmail.db");
 const countQuery = db.prepare('SELECT COUNT(*) AS total FROM users');
 
 export default async function updateUsercount() {
     for(;;) {
         const result = countQuery.get();
-        const countString = result.total.toString();
         try {
-            const __filename = fileURLToPath(import.meta.url);
-            const __dirname = path.dirname(__filename);
-            const filePath = path.join(__dirname, "usercount.txt")
-            await writeFile(filePath,countString);
+            await writeFile(filePath, result.total.toString());
         } catch(err) {
-            console.log(err);
+            console.error('Polling error:', err);
         }
-        await new Promise((r) => setTimeout(r, 0.5*1000*60*60));
+        await new Promise((r) => setTimeout(r, 0.5 * 1000 * 60 * 60));
     }
 }
