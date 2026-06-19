4335444fc66707d46e48a88c4496edd976ecec96
András Simon <andrex@andrexserver.org>
2026-06-19 15:40:25 +0200

    fix imports in admin.js

--- admin.js
+++ admin.js
@@ -1,13 +1,11 @@
-const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
-const { SqliteGuiNodeMiddleware } = require("sqlite-gui-node");
+import express from express
+import sqlite3 from sqlite3.verbose()
+import { SqliteGuiNodeMiddleware } from "sqlite-gui-node";
 
 const PORT = 8082;
 const adminApp = express();
-// Point this to your existing SQLite file
 const db = new sqlite3.Database("./shadowmail.db"); 
 
-// Pass your admin app instance and database connection
 adminApp.use(SqliteGuiNodeMiddleware(adminApp, db));
 
 adminApp.listen(PORT, () => {
