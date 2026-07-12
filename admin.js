522a282adc498e1f0d91787986f89d3f2ea7cbe5
András Simon <andrex@andrexserver.org>
2026-07-13 00:58:04 +0200

    fix admin page

--- admin.js
+++ admin.js
@@ -1,13 +1,10 @@
-import express from "express"
-import sqlite3 from "sqlite3"
-import { SqliteGuiNodeMiddleware } from "sqlite-gui-node";
-
 const PORT = 8082;
+const HOST = "0.0.0.0";
 const adminApp = express();
-const db = new sqlite3.Database("./shadowmail.db"); 
+const db = new sqlite3.Database("./shadowmail.db");
 
 adminApp.use(SqliteGuiNodeMiddleware(adminApp, db));
 
-adminApp.listen(PORT, () => {
-  console.log(`database manager running at http://localhost:${PORT}/home`);
+adminApp.listen(PORT, HOST, () => {
+  console.log(`database manager running at http://192.168.1.20:${PORT}/home`);
 });
